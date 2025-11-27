22817a20cb9828682cc536e9b65d53e77b02b986
Denis Farr <denis@spawnlink.eu>
2025-11-27 15:06:25 +0200

    change layout

--- dWldZ/keymap.c
+++ dWldZ/keymap.c
@@ -43,7 +43,7 @@ const tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     KC_TRNS ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,KC_AVLD ,
-                                                                        KC_AVUP ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_10   ,KC_TRNS ,
+                                                                        KC_AVUP ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_TRNS ,
     KC_ESCP ,KC_TRNS ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_BRDN ,
                                                                         KC_BRUP ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_TRNS ,KC_TRNS ,
     KC_TAB  ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_HYPR ,
